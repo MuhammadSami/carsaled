305695467ce2139da3f6306b5e9eacc1c6608102
wallid <wallidhajj@gmail.com>
2018-10-09 22:10:14 +1100

    (feature): create basic vehicle controller and models

---
+++ Carsales.Web/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Carsales.Web.Models
+{
+    public class Car : Vehicle
+    {
+        public string Engine { get; set; }
+        public int Doors { get; set; }
+        public int Wheels { get; set; }
+        public BodyType BodyType { get; set;}
+    }
+}
